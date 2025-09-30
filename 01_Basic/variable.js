51c54b21e65dbf78aca5b989838d8667773cb65a
Kashfeet Raza <kashfeetraza@gmail.com>
2025-09-30 21:53:17 +0000

    variable and datatypes

---
+++ 01_Basic/variable.js
@@ -0,0 +1,59 @@
+console.log("This is video 55 where we study about variables in javaScript");
+
+var a = 2;
+var b = 4;
+var c = "JARRY";
+
+console.log(a);
+console.log(b);
+
+
+console.log(a + b);
+console.log(c);
+
+// if i want to find out the type of data we can use the typeof commend
+
+console.log(typeof a, typeof b, typeof c);
+
+
+// Constant
+
+const a3 = 4;
+
+a3 + 2;
+
+console.log(a3);
+
+
+// let
+
+// we use let is block commend
+
+let a4 = 5;
+{
+    let a4 = 4;
+    console.log(a4);
+    
+}
+console.log(a4);
+
+// Primitive Datatype
+ 
+let x = "ali";
+let y = 4;
+let z = 4.5;
+let de = true;
+let df = undefined;
+let dh = null;
+
+console.log(x, y, z, de, df, dh);
+
+console.log(typeof x, typeof y, typeof z, typeof de, typeof df, typeof dh);
+
+let obj = {
+    "name": "ali",
+    "home!": 399,
+    "sfg":43
+
+}
+console.log(obj);
