14a1c9add54f14aa285b7cef2239a5cd11318831
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-01 17:02:55 +0000

    today done

---
+++ 01_Basic/conditional.js
@@ -0,0 +1,48 @@
+let a = 17;
+let g = 2;
+if(a>18){
+    console.log("You Can Drive");
+}
+else if((a+g)>15){
+    console.log("You cannot Drive");
+}else{
+    console.log("you are child");
+}
+
+console.log(a + g);
+
+
+let a = 12;
+let g = 4;
+
+console.log(a + g);
+console.log(a - g);
+console.log(a * g);
+console.log(a / g);
+console.log(a ** g);
+console.log(a % g);
+
+// Assingment Operator
+
+let x = 3;
+let y = 2;
+
+console.log(x = y);
+console.log(x += y);
+console.log(x -= y);
+console.log(x *= y);
+console.log(x **= y);
+
+
+// comperison Operator
+let o = 3;
+let h = 2;
+console.log(o==h);
+console.log(o!=h);
+console.log(o===h);
+console.log(o!==h);
+console.log(o>h);
+console.log(o<h);
+console.log(o<=h);
+console.log(o>=h);
+
