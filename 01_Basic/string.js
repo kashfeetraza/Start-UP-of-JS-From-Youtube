efa44276de1b5e5a9f0ad3fccd895e0e7a91c89b
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-03 16:37:27 +0000

    Day 2 JavaScript Done

---
+++ 01_Basic/string.js
@@ -0,0 +1,76 @@
+// let a = "Kash Raza";
+
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[5]);
+// console.log(a[6]);
+
+// console.log(a.length);
+
+
+let yourName = "Ali";
+let friendName = "Shebaz";
+
+// console.log("Your Name is " + yourName + " Your Friend Name is " + friendName );
+
+// console.log(`your name is ${yourName} and your friend name is ${friendName}`);
+
+// Escape Sequence in javascript
+
+// if i want to write conde in double quotes its produce error
+
+// console.log("ali\"raza"); //for qoutes
+// console.log("ali\nraza"); //for add new line
+
+// string Properties and Method
+
+// let a = "ali";
+
+// console.log("touppercase");
+// console.log(a.toUpperCase());
+
+// console.log("toLowercase");
+// console.log(a.toLowerCase());
+
+// console.log("toLocallowercase");
+// console.log(a.toLocaleLowerCase());
+
+// console.log("toLocaluppercase");
+// console.log(a.toLocaleUpperCase());
+
+// console.log("tostring");
+// console.log(a.toString());
+
+// console.log("towellformed");
+// console.log(a.toWellFormed());
+
+
+// Slice Function
+
+// let a = "Kashfeetraza"
+// console.log(a.slice(5,7));
+
+
+let a = "Kashfeetraza"
+let b = "shakeel"
+console.log(a.replace("Ka","Sa"));
+
+console.log(a.concat(b," raza"," harry"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
