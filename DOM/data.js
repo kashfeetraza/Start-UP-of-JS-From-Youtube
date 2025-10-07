0378368bc8130ed4eb7afb868789afe38a73286c
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-07 01:59:50 +0000

    Done

---
+++ DOM/data.js
@@ -0,0 +1,35 @@
+console.log("Hellow World");
+
+console.log("Hellow World");
+
+// Hellow World
+document.body
+
+document.bo
+undefined
+document.body.childNodes
+NodeList(5) [text, div.container, text, script, text]
+document.body.childNodes[0]
+
+// #text
+document.body.childNodes[1]
+
+
+
+// #text
+document.body.childNodes[3]
+
+
+document.body.childNodes[1].childNodes
+
+
+let jss = document.body.childNodes[1]
+undefined
+jss.lastElementChild.style.backgroundColor = "green";
+'green'
+jss.childNodes[3].sty
+undefined
+jss.childNodes[3].style.color = "red"
+'red'
+jss.childNodes[3].style.backgroundColor = "grey"
+'grey'
