423781db643d3f108c8fa5204b60a308df7f6f3e
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-07 21:02:57 +0000

    Study of conversion

---
+++ conversions/inde.js
@@ -0,0 +1,20 @@
+let yourScore = false
+
+//console.log(typeof yourScore);
+//console.log(typeof (yourScore));
+
+let valueInNumber = Number(yourScore)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let CheckBoolien = "hello"
+let changeItBoolien = Boolean(CheckBoolien)
+
+console.log(typeof changeItBoolien);
+console.log(changeItBoolien);
+
+
+
+
+
+
