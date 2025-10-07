cde0cdf9ef8437f1b0f324d419b6489867d35f9f
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-07 21:41:25 +0000

    Operators

--- conversions/inde.js
+++ conversions/inde.js
@@ -10,11 +10,49 @@ let valueInNumber = Number(yourScore)
 let CheckBoolien = "hello"
 let changeItBoolien = Boolean(CheckBoolien)
 
-console.log(typeof changeItBoolien);
-console.log(changeItBoolien);
+//console.log(typeof changeItBoolien);
+//console.log(changeItBoolien);
+
+
+//------------------------Operation--------------------------
+
+
+let value1 = 3
+let value2 = 5
+
+// console.log("Sum of Value1 and Value2:",value1+value2);
+// console.log("Multiple of Value1 and Value2:",value1*value2);
+// console.log("Divide of Value1 and Value2:",value1/value2);
+// console.log("Reminder of Value1 and Value2:",value1%value2);
+// console.log("Expotational of Value1 and Value2:",value1**value2);
 
 
 
+let str1 = "Ali"
+let str2 = " Raza"
+
+// console.log(str1+str2);
+
+/*
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + "2" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+*/
+
+
+let num1 , num2, num3
+num1 = num2 = num3 = 4
+
+// console.log(num3);
+
+// Prefix and Postfix
+
+let PrayerCounter = 100
+console.log(PrayerCounter);
+PrayerCounter++;
+console.log(PrayerCounter);
 
 
 
