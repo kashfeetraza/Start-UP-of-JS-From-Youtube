81bb0ee830f2ed6965dda4a2e8ee64f6c190b902
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-11 21:03:06 +0500

    Create script2.js

---
+++ Day 11/script2.js
@@ -0,0 +1,12 @@
+let age = 20;
+
+if (age < 13) {
+  console.log("You are a child.");
+} else if (age >= 13 && age < 18) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are an adult.");
+}
+
+let isStudent = true;
+console.log(isStudent ? "Student discount available." : "No discount available.");
