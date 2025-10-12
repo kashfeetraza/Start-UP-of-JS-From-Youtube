dcb91172f0623b306f43fdae85119b8e610b55ff
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-12 20:49:00 +0500

    Update and rename index2.html to script2.js

---
+++ Day 12/script2.js
@@ -0,0 +1,11 @@
+console.log("5" == 5);   // true
+console.log("5" === 5);  // false (type check)
+console.log(0 == false); // true
+console.log(0 === false);// false
+
+console.log(null == undefined);  // true
+console.log(null === undefined); // false
+
+let a = 10, b = "10";
+console.log(a != b);   // false
+console.log(a !== b);  // true
