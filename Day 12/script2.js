9548ce1580c6f2826eccf7a03b2c400b949d9542
Kashfeet Raza <kashfeetraza@gmail.com>
2025-10-12 20:49:48 +0500

    Update and rename script.js to script2.js

---
+++ Day 12/script2.js
@@ -0,0 +1,16 @@
+const city = "Karachi";
+const country = "Pakistan";
+
+console.log(`I live in ${city}, which is in ${country}.`);
+
+const sentence = "JavaScript is awesome and powerful";
+console.log(sentence.toUpperCase());
+console.log(sentence.includes("awesome"));
+
+const words = sentence.split(" ");
+console.log(words);
+
+const fruits = ["apple", "banana", "mango"];
+fruits.push("orange");
+console.log(fruits);
+console.log(fruits.join(" - "));
