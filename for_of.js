468c765fdb7df19fb77aa7f59059b35ceb2e3439
Kashfeet Raza <kashfeetraza@gmail.com>
2026-03-15 21:48:17 +0500

    Add files via upload

---
+++ for_of.js
@@ -0,0 +1,15 @@
+const arr = [ 1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num)
+}
+
+const stringarr = "Kashfeet"
+
+for (const name of stringarr) {
+    console.log(`Each Character is ${name}`);
+    
+}
+
+// 5:58 Youtube Time Video High Order (v:29)
+
